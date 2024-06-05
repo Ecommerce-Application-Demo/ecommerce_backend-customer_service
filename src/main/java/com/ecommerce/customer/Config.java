232b623cf5e35a9b382c7694b2e2d822fb57e9d1
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-06-05 20:22:36 +0530

    - Implemented Redis based login for less db calls & faster response.

--- src/main/java/com/ecommerce/customer/Config.java
+++ src/main/java/com/ecommerce/customer/Config.java
@@ -1,17 +1,26 @@
 package com.ecommerce.customer;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableScheduling
 public class Config {
+
+	@Autowired
+	Environment env;
 
     @Bean
     ModelMapper modelMapper() {
@@ -30,4 +39,11 @@ public class Config {
 	              .url("https://github.com/Ecommerce-Application-Demo/ecommerce-backend"));
 	  }
 
+	@Scheduled(fixedDelay = 1000*60*5)
+	void renderRunner() {
+		if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
+			RestTemplate restTemplate = new RestTemplate();
+			restTemplate.getForEntity("https://ecommerce-backend-customer-service.onrender.com/user/api/auth/index", String.class);
+		}
+	}
 }
