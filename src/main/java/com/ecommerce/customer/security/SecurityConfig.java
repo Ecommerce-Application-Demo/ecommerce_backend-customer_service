7b8a3a604b7d4266dfa2c6c6ff399a2ede32eeeb
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-09-19 19:25:56 +0530

    Fixed Cors issue

--- src/main/java/com/ecommerce/customer/security/SecurityConfig.java
+++ src/main/java/com/ecommerce/customer/security/SecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -67,11 +66,6 @@ public class SecurityConfig {
 				.authenticationProvider(authenticationProvider())
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
-	}
-
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring().requestMatchers(publicURL);
 	}
 
 	@Bean
